25a7ba593b75524c79e2571ec78d1088a2ce04dd
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-05 12:04:14 -0400

    Finish styling the Form component and add randomnes to picking author, chapter and percentage

--- src/components/books/Book.js
+++ src/components/books/Book.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../../redux/books/books';
@@ -8,18 +8,46 @@ const Book = ({ book }) => {
   const { id, title, category } = book;
   const dispatch = useDispatch();
 
+  const [percentage, setPercentage] = useState();
+  const [chapter, setChapter] = useState();
+  const [bookAuthor, setBookAuthor] = useState();
+
+  const getRandomInt = (max) => (Math.floor(Math.random() * max));
+
+  const setPercentageAndChapter = () => {
+    const randomPercentage = getRandomInt(100);
+    const randomChapter = getRandomInt(40);
+
+    setPercentage(randomPercentage);
+    setChapter(randomChapter);
+  };
+
+  const pickingBookAuthor = () => {
+    const bookAuthorList = [
+      'Marcel Proust', 'James Joyce', 'Miguel de Cervantes', 'Gabriel Garcia Marquez', 'Scott Fitzgerald', 'Herman Melville', 'Leo Tolstoy', 'William Shakespeare', 'Homer', 'Gustave Flaubert', 'Dante Alighieri', 'Vladimir Nabokov', 'Fyodor Dostoyevsky', 'Emily Bronte', 'J.D. Salinger', 'Jane Austen', 'Mark Twain', 'Lewis Carroll', 'Virginia Woolf', 'Joseph Heller', 'William Faulkner', 'George Orwell', 'Charles Dickens', 'John Steinbeck', 'Ralph Ellison', 'Harper Lee', 'Franz Kafka', 'Stendhal', 'George Eliot', 'Jonathan Swift', 'Toni Morrison', 'Anton Chekhov', 'Albert Camus', 'Charlotte Bronte', 'Virgil', 'Jorge Luis Borges', 'Ernest Hemingway', 'Laurence Sterne', 'Walt Whitman', 'Thomas Mann', 'James Joyce', 'Salman Rushdie',
+    ];
+
+    const randomAuthorNumber = getRandomInt(42);
+    const author = bookAuthorList[randomAuthorNumber];
+    setBookAuthor(author);
+  };
+
+  useEffect(() => {
+    setPercentageAndChapter();
+    pickingBookAuthor();
+  }, []);
+
   const handleRemoveBook = () => {
     dispatch(removeBook(id));
   };
 
   return (
     <div className={classes.card}>
-
       <div>
         <p className={classes.bookInfoContainer}>
           <span className={classes.bookCategory}>{category}</span>
           <span className={classes.bookTitle}>{title}</span>
-          <span className={classes.bookAuthor}>F. Scott Fitzgerald</span>
+          <span className={classes.bookAuthor}>{bookAuthor}</span>
         </p>
 
         <p className={classes.buttonsContainer}>
@@ -33,14 +61,21 @@ const Book = ({ book }) => {
 
         <div className={classes.loader} />
         <p className={classes.percentageContainer}>
-          <span className={classes.percentage}>64%</span>
+          <span className={classes.percentage}>
+            {percentage}
+            %
+          </span>
           <span className={classes.completed}>Completed</span>
         </p>
       </div>
 
       <div className={classes.chapterContainer}>
         <span className={classes.currentChapter}>Current Chapter</span>
-        <span className={classes.chaper}>Chapter 17</span>
+        <span className={classes.chapter}>
+          Chapter
+          {' '}
+          {chapter}
+        </span>
         <button className={classes.updateProgress} type="button">UPDATE PROGRESS</button>
       </div>
 
